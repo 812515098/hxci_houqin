49240d1b59f503ed72806b46a3900c643210225e
wj1994-wj <1099488566@qq.com>
2020-08-03 17:44:07 +0800

    账号设置
    0-5我的-账号设置-详情手机

---
+++ app/src/main/java/com/example/myapplication/SettingActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingActivity extends AppCompatActivity {
+    TextView title;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.seting);
+
+    }
+
+
+
+
+}
